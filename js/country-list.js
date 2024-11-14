69a549419e442f23dcbcdb64df5ad7444abdffdb
Tiago Dias <80550646+tiagofdias@users.noreply.github.com>
2024-11-14 19:05:59 +0000

    Add files via upload

---
+++ js/country-list.js
@@ -0,0 +1,48 @@
+let country_list = {
+    "USD": "US",  // United States Dollar
+    "EUR": "FR",  // Euro
+    "JPY": "JP",  // Japanese Yen
+    "GBP": "GB",  // British Pound Sterling
+    "AUD": "AU",  // Australian Dollar
+    "CAD": "CA",  // Canadian Dollar
+    "CHF": "CH",  // Swiss Franc
+    "CNY": "CN",  // Chinese Yuan
+    "HKD": "HK",  // Hong Kong Dollar
+    "NZD": "NZ",  // New Zealand Dollar
+    "SEK": "SE",  // Swedish Krona
+    "KRW": "KR",  // South Korean Won
+    "SGD": "SG",  // Singapore Dollar
+    "NOK": "BV",  // Norwegian Krone
+    "MXN": "MX",  // Mexican Peso
+    "INR": "IN",  // Indian Rupee
+    "RUB": "RU",  // Russian Ruble
+    "ZAR": "ZA",  // South African Rand
+    "TRY": "TR",  // Turkish Lira
+    "BRL": "BR",  // Brazilian Real
+    
+    // Remaining currencies in alphabetical order
+    "AFN": "AF", "XCD": "AG", "ALL": "AL", "AMD": "AM", "ANG": "AN", 
+    "AOA": "AO", "AQD": "AQ", "AZN": "AZ", "BAM": "BA", "BBD": "BB",
+    "BIF": "BI", "BMD": "BM", "BND": "BN", "BOB": "BO", "BSD": "BS",
+    "BWP": "BW", "BYR": "BY", "BZD": "BZ", "CDF": "CD", "XAF": "CF",
+    "CLP": "CL", "CRC": "CR", "CUP": "CU", "CVE": "CV", "CYP": "CY",
+    "DJF": "DJ", "DOP": "DO", "DZD": "DZ", "ECS": "EC", "EEK": "EE",
+    "ETB": "ET", "FJD": "FJ", "FKP": "FK", "GEL": "GE", "GGP": "GG",
+    "GHS": "GH", "GIP": "GI", "GMD": "GM", "GNF": "GN", "GTQ": "GT",
+    "GYD": "GY", "HNL": "HN", "HRK": "HR", "HTG": "HT", "ISL": "IS",
+    "JMD": "JM", "JOD": "JO", "KES": "KE", "KGS": "KG", "KHR": "KH",
+    "KMF": "KM", "KPW": "KP", "KYD": "KY", "KZT": "KZ", "LAK": "LA",
+    "LBP": "LB", "LKR": "LK", "LRD": "LR", "LSL": "LS", "LTL": "LT",
+    "LVL": "LV", "LYD": "LY", "MAD": "MA", "MDL": "MD", "MGA": "MG",
+    "MKD": "MK", "MMK": "MM", "MNT": "MN", "MOP": "MO", "MRO": "MR",
+    "MTL": "MT", "MUR": "MU", "MVR": "MV", "MWK": "MW", "MZN": "MZ",
+    "NAD": "NA", "XPF": "NC", "NGN": "NG", "NIO": "NI", "NPR": "NP",
+    "PAB": "PA", "PEN": "PE", "PGK": "PG", "PYG": "PY", "RSD": "RS",
+    "RWF": "RW", "SBD": "SB", "SCR": "SC", "SDG": "SD", "SLL": "SL",
+    "SOS": "SO", "SRD": "SR", "STD": "ST", "SVC": "SV", "SYP": "SY",
+    "SZL": "SZ", "TJS": "TJ", "TMT": "TM", "TND": "TN", "TOP": "TO",
+    "TTD": "TT", "TZS": "TZ", "UAH": "UA", "UGX": "UG", "UYU": "UY",
+    "UZS": "UZ", "VEF": "VE", "VUV": "VU", "YER": "YE", "ZMK": "ZM",
+    "ZWD": "ZW"
+};
+
---
+++ js/script.js
@@ -0,0 +1,56 @@
+const dropList = document.querySelectorAll("form select"),
+fromCurrency = document.querySelector(".from select"),
+toCurrency = document.querySelector(".to select"),
+getButton = document.querySelector("form button");
+
+for (let i = 0; i < dropList.length; i++) {
+    for(let currency_code in country_list){
+        // selecting USD by default as FROM currency and AFN as TO currency
+        let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "AFN" ? "selected" : "";
+        // creating option tag with passing currency code as a text and value
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+        // inserting options tag inside select tag
+        dropList[i].insertAdjacentHTML("beforeend", optionTag);
+    }
+    dropList[i].addEventListener("change", e =>{
+        loadFlag(e.target); // calling loadFlag with passing target element as an argument
+    });
+}
+
+window.addEventListener("load", ()=>{
+    getExchangeRate();
+});
+
+getButton.addEventListener("click", e =>{
+    e.preventDefault(); //preventing form from submitting
+    getExchangeRate();
+});
+
+const exchangeIcon = document.querySelector("form .icon");
+exchangeIcon.addEventListener("click", ()=>{
+    let tempCode = fromCurrency.value; // temporary currency code of FROM drop list
+    fromCurrency.value = toCurrency.value; // passing TO currency code to FROM currency code
+    toCurrency.value = tempCode; // passing temporary currency code to TO currency code
+    getExchangeRate(); // calling getExchangeRate
+})
+
+function getExchangeRate(){
+    const amount = document.querySelector("form input");
+    const exchangeRateTxt = document.querySelector("form .exchange-rate");
+    let amountVal = amount.value;
+    // if user don't enter any value or enter 0 then we'll put 1 value by default in the input field
+    if(amountVal == "" || amountVal == "0"){
+        amount.value = "1";
+        amountVal = 1;
+    }
+    exchangeRateTxt.innerText = "Getting exchange rate...";
+    let url = `https://v6.exchangerate-api.com/v6/e9b862270ccfde0f9e17ec92/latest/${fromCurrency.value}`;
+    // fetching api response and returning it with parsing into js obj and in another then method receiving that obj
+    fetch(url).then(response => response.json()).then(result =>{
+        let exchangeRate = result.conversion_rates[toCurrency.value]; // getting user selected TO currency rate
+        let totalExRate = (amountVal * exchangeRate).toFixed(2); // multiplying user entered value with selected TO currency rate
+        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+    }).catch(() =>{ // if user is offline or any other error occured while fetching data then catch function will run
+        exchangeRateTxt.innerText = "Something went wrong";
+    });
+}
